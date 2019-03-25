fef311ae723e018856817ee8f4a9f9ef69f0f8ed
jingyu1807 <787641502@qq.com>
2019-03-25 15:08:08 +0800

    注册

--- serice-feign/src/main/java/com/lyq/service/TestService.java
+++ serice-feign/src/main/java/com/lyq/service/TestService.java
@@ -27,6 +27,6 @@ public interface TestService {
     void alipay(@RequestBody String money);
     @RequestMapping(value = "pack/queryPackages",method = RequestMethod.GET)
     List queryPa();
-    @RequestMapping(value = "sitesUser/sendMessage",consumes = "application/json")
+    @RequestMapping(value = "sitesUser/sendMessage",method = RequestMethod.GET)
     void sendMessage();
 }
